61f97d53bb88d29ef56b93114be4a641c3f2895a
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-19 17:42:16 +0200

    completamento

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 
-let Eta = prompt('Scrivimi quanti anni hai:');
-console.log(latuaeta);
+let eta = prompt('Scrivimi quanti anni hai:');
+console.log(eta);
 
-let Chilometri = prompt('Scrivimi i chilometri che vuoi fare:');
-console.log(chilometrichevuoifare);
+let chilometri = prompt('Scrivimi i chilometri che vuoi fare:');
+console.log(chilometri);
 
     const priceKm = 0.21;
 
-    let prezzototale = Chilometri * priceKm;
+    let ticketPrice = chilometri * priceKm;
 
     let underdiscount = ticketPrice * 0.2;
 
     let upperdiscount = ticketPrice * 0.4;
 
     const ticketKidPrice = ticketPrice - underdiscount;
-    const ticketSeniorPrice = ticketKidPrice - upperdiscount;
+    const ticketSeniorPrice = ticketPrice - upperdiscount;
 
-    if (Eta >= 65){
+    if (eta >= 65){
         ticketPrice = ticketSeniorPrice.toFixed(2);
     }
     
-    else if (Eta <= 18){
+    else if (eta <= 18){
         ticketPrice = ticketKidPrice.toFixed(2);
     }
 
     else{
-        ticketPrice = ticketPrice.toFixed(2);
+        prezzototale = prezzototale.toFixed(2);
     }
 
     document.getElementById('prezzototale').innerHTML = "prezzototale " + ticketPrice+ "$"
